9d622fcdda9506fdec710286209f00d12b0ca8bc
reagan718 <reagan.s.roque@accenture.com>
2025-12-17 11:00:21 +0000

    Update ALLOWED_HOSTS for codespace and localhost support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure-438dlsrib1*cbnond)vvz06l$vw@+)b*562mwn@7l*x^ulyc1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+# Allow localhost and codespace URL
+codespace_name = os.environ.get("CODESPACE_NAME")
+allowed_hosts = ["localhost", "127.0.0.1"]
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
